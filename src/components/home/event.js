912f2fe0209830b846d725e0f0ec5d306c58bbea
Izyne Gallardo <295115483+izynegallardo@users.noreply.github.com>
2026-08-11 14:40:41 +0800

    removed ping in homepage

--- src/components/home/event.js
+++ src/components/home/event.js
@@ -1,13 +1,3 @@
-import ping from '@/utils/ping'
-
 export default async function Events() {
-    try {
-        await ping()
-    } catch (error) {
-        document.querySelector('#under-maintenance').style.display = 'flex'
-        document.querySelector('#app').style.display = 'none'
-        return
-    }
-
     // TODO: if user is already logged in, redirect to /dashboard
 }
